801fbbd1756d849641f3c45c6ec1f6142748e7d4
kgupta786 <iitengineer786@gmail.com>
2018-12-21 14:04:49 -0500

    Initial Files of CloudBase Project

---
+++ indexmain.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>www.cloudbase.com</title>
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="icons.css" rel="stylesheet" />
+</head>
+
+<body>
+
+
+<div class="container-fluid">
+<div class="row" style="min-height: 29mm;background-color: #01579B;">
+<div class="col-lg-2" style="min-height: 30mm">
+
+</div>
+
+<div class="col-lg-4" style="min-height: 30mm">
+<input type="search" class="form-control" style="width: 140mm;height: 16mm;margin-top: 6mm;margin-left: 10mm;border-radius: 8mm;outline: 0; 
+font-size: 7mm;font-family: Lato; padding-left: 5mm; outline-color: black;" placeholder="Search Your Cloudbase here..." />
+</div>
+
+<div class="col-lg-3" style="min-height: 30mm">
+
+<input type="button" class="btn" value="SignUp" style="background-color: white; width: 40mm;height: 16mm;border-radius: 8mm;margin-top: 6mm;
+margin-left:10mm ;outline: 0;font-size: 5mm; font-family: Lato"  />
+
+<input type="button" class="btn" value="Login" style="background-color: white;width: 40mm;height: 16mm;border-radius: 8mm;margin-top: 6mm;
+margin-left:7mm ;outline: 0;font-size: 5mm; font-family: Lato"  />
+</div>
+
+
+
+<div class="col-lg-3" style="min-height: 30mm">
+
+<p style=" margin-top: 3mm; font-size:16mm ;  font-family: Lato;text-align: center;color: white">Cloudbase..</p>
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+<div class="row" style="height:170mm"></div>
+
+<div class="row" style="height:160mm;background-color: #F7B529">
+<div class="col-lg-4" style="height: 160mm" >
+<img src="w1.png" style="height: 160mm;border-color: black;">
+</div>
+
+<div class="col-lg-4" style="height: 160mm" >
+<img src="w3.png" style="height: 159mm;border-color: black;">
+</div>
+
+<div class="col-lg-4" style="height: 160mm;background-image: url(w2.png);">
+<input type="button" class="btn" value="SignUp" style="background-color: black; width: 40mm;height: 16mm;border-radius: 8mm;margin-top: 12mm;
+margin-left:20% ;outline: 0;font-size: 7mm; font-family: Lato;color:#F7B529;"  />
+<input type="button" class="btn" value="Login" style="background-color: black; width: 40mm;height: 16mm;border-radius: 8mm;margin-top: 12mm;
+margin-left:7mm ;outline: 0;font-size: 7mm; font-family: Lato; color:#F7B529 "  />
+<hr style="width: 60mm;" >
+
+<input type="email" style=" color:white;  margin-left:12% ;margin-top: 5mm; width: 120mm; height: 18mm;border-radius: 9mm;outline: 0;
+font-size: 6mm;padding-left: 5mm" placeholder="Email-Address" class="form-control"  />
+
+
+
+<input type="tel" style="  color:white;margin-left:12% ; margin-top: 5mm; width: 120mm; height: 18mm;border-radius: 9mm;outline: 0;
+font-size: 6mm;padding-left: 5mm" placeholder="Mobile No. " class="form-control"  />
+
+
+<input type="password" style=" color:white;margin-left:12% ; margin-top: 5mm; width: 120mm; height: 18mm;border-radius: 9mm;outline: 0;
+font-size: 6mm;padding-left: 5mm" placeholder="Password" class="form-control"  />
+
+
+<input type="password" style=" color:white;margin-left:12% ;margin-top: 5mm; width: 120mm; height: 18mm;border-radius: 9mm;outline: 0;
+font-size: 6mm;padding-left: 5mm" placeholder="Confirm Password" class="form-control"  />
+
+
+<input type="button" class="btn" value="Submit Details" style="background-color: black; width: 70mm;height: 16mm;border-radius: 8mm;margin-top: 12mm;
+margin-left:24% ;outline: 0;font-size: 7mm; font-family: Lato;color:#F7B529;"  />
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+<div class="row" style="background-color: white;height: 1mm"></div>
+
+<div class="row" style="height:30mm;background-color: #01579B;">
+
+<div class="col-lg-5" style="height: 30mm" >
+<p style="font-size: 10mm;font-family: Lato;margin-top: 7mm;margin-left: 10mm;color:  white;">Designed By : &nbsp;Keshav Gupta</p>
+
+</div>
+
+<div class="col-lg-7" style="height: 30mm" >
+<a href="#" class="fa fa-facebook" style="margin-left: 65%;margin-top: 4mm;color: white;" ></a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="#" class="fa fa-linkedin" style="margin-top: 4mm;color: white;"></a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="#" class="fa fa-google"style="margin-top: 4mm;color: white;" ></a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="#" class="fa fa-instagram" style="margin-top: 4mm;color: white;"></a>
+
+</div>
+
+</div>
+
+
+
+</div>
+
+
+
+
+</body>
+
+
+
+
+</html>
+
+
+
